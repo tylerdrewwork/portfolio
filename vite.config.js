845d4fc935e34952560d5cecc0f95250848233cb
tylerdrewwork <33964795+tylerdrewwork@users.noreply.github.com>
2026-02-01 18:18:36 -0600

    Sets base URL for GitHub Pages deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio-2026/', // GitHub Pages: https://<user>.github.io/portfolio-2026/
   plugins: [
     react({
       babel: {
