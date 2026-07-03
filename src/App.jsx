9979272175844e44efe33dbafbf2c9125ba9076e
tylerdrewwork <33964795+tylerdrewwork@users.noreply.github.com>
2026-07-03 11:48:39 -0500

    Add react-router-dom for routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 
@@ -5,7 +6,10 @@ function App() {
   return (
     <div className="app">
       {/* <Header /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/escapely" element={<Home />} />
+      </Routes>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/portfolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
